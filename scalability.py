2d0b8312e8c0629fcc358fc530202597a5481344
Guillermo Cornejo Suárez <gcornejo@cenat.ac.cr>
2018-01-12 10:43:59 -0600

    Full and single test ready

--- scalability.py
+++ scalability.py
@@ -7,12 +7,10 @@ import matplotlib.pyplot as plt
 import sys
 import modules 
 
-import __main__ # you sure??
-
 def dummy_setup():
     pass
 
-def poli_growth(coefficient, order):
+def poli_growth(base, order):
     return lambda c: base * c**order
 
 class Executable:
@@ -39,7 +37,9 @@ def tester(executable, G, cores, repetitions):
     for r in range(repetitions):
         series = []
         for c in cores:
-            series.append(executable.run(c, G(c)))
+            base_run = executable.run(cores[0], G(c))
+            test_run = executable.run(c, G(c))
+            series.append(base_run / test_run)
         reps.append(np.array(series))
     return np.array(reps)
 
@@ -48,27 +48,26 @@ def scaling_sigle_test(executable, G, cores, repetitions, print_times=False):
     if print_times:
         np.savetxt(executable.name.replace(' ', '_') + '_scaling_test.txt', scaling)
 
-    cores = np.array([np.prod(c) for c in cores[1:]])
+    workers = np.array([np.prod(c) for c in cores])
     cores_label = [str(c) for c in cores]
 
-    scaling = (scaling[:,0] / scaling.T)[1:]
-    speedup = np.mean(scaling, axis=1) 
-    speedup_std = np.std(scaling, axis=1) 
-    efficiency = speedup / cores
+    speedup = np.mean(scaling, axis=0) 
+    speedup_std = np.std(scaling, axis=0) 
+    efficiency = speedup / workers
 
     fig, ax1 = plt.subplots()
     ax1.set_xscale('log', basex=2)
     ax1.set_title(executable.name + ' scalablity')
     ax1.set_xlabel('Workers')
-    plt.xticks(cores, cores_label)
+    plt.xticks(workers, cores_label)
 
     ax1.set_ylabel('Speedup')
-    ax1.errorbar(cores, speedup, fmt='-', yerr=speedup_std, label='Speedup')
+    ax1.errorbar(workers, speedup, fmt='-', yerr=speedup_std, label='Speedup')
 
     ax2 = ax1.twinx()
     ax2.set_ylabel('Efficiency')
     ax2.set_ylim([0,1])
-    ax2.plot(cores, efficiency, '--', label='Efficiency')
+    ax2.plot(workers, efficiency, '--', label='Efficiency')
 
     ax1.legend(loc=2)
     ax2.legend(loc=1)
@@ -76,52 +75,50 @@ def scaling_sigle_test(executable, G, cores, repetitions, print_times=False):
     fig.savefig(executable.name.replace(' ', '_') + '_scaling.png')
 
 def scaling_full_test(executable, G_strong, G_weak, cores, repetitions, print_times=False):
-    strong = tester(executalbe, G_strong, cores, repetitions)
-    weak   = tester(executalbe, G_weak, cores, repetitions)
+    strong_scaling = tester(executable, G_strong, cores, repetitions)
+    weak_scaling   = tester(executable, G_weak, cores, repetitions)
     if print_times:
-        np.savetxt(executable.name.replace(' ', '_') + '_strong.png', strong)
-        np.savetxt(executable.name.replace(' ', '_') + '_weak.png', weak)
+        np.savetxt(executable.name.replace(' ', '_') + '_strong.txt', strong_scaling)
+        np.savetxt(executable.name.replace(' ', '_') + '_weak.txt', weak_scaling)
 
-    cores = np.array([np.prod(c) for c in cores[1:]])
+    workers = np.array([np.prod(c) for c in cores])
     cores_label = [str(c) for c in cores]
 
-    strong_scaling = (strong[:,0] / strong.T)[1:]
-    strong_speedup = np.mean(strong_scaling, axis=1)
-    strong_speedup_std = np.std(strong_speedup, axis=1)
-    strong_efficiency = strong_speedup / cores
+    strong_speedup = np.mean(strong_scaling, axis=0)
+    strong_speedup_std = np.std(strong_scaling, axis=0)
+    strong_efficiency = strong_speedup / workers
 
-    weak_scaling = (weak[:,0] / weak.T)[1:]
-    weak_speedup = np.mean(weak_scaling, axis=1)
-    weak_speedup_std = np.std(weak_speedup, axis=1)
-    weak_efficiency = weak_speedup / cores
+    weak_speedup = np.mean(weak_scaling, axis=0)
+    weak_speedup_std = np.std(weak_scaling, axis=0)
+    weak_efficiency = weak_speedup / workers
 
     fig, ax1 = plt.subplots()
     ax1.set_xscale('log', basex=2)
-    ax1.set_tile(executable.name + 'scalability')
+    ax1.set_title(executable.name + 'scalability')
     ax1.set_xlabel('Workers')
-    plt.xticks(cores, cores_label)
+    plt.xticks(workers, cores_label)
 
     ax2 = ax1.twinx()
     ax2.set_ylim([0,1])
     ax1.set_ylabel('Speedup') 
     ax2.set_ylabel('Efficiency')
 
-    ax1.errorbar(cores, strong_speedup, fmt='-', yerr=strong_speedup_std, label='Strong speedup')
-    ax1.errorbar(cores, weak_speedup, fmt='--', yerr=weak_speedup_std, label='Weak efficiency')
-    ax2.plot(cores, strong_efficiency, '-.', label='Strong efficiency')
-    ax2.plot(cores, weak_efficiency, ':', label='Weak efficiency')
+    ax1.errorbar(workers, strong_speedup, fmt='-', yerr=strong_speedup_std, label='Strong speedup')
+    ax1.errorbar(workers, weak_speedup, fmt='--', yerr=weak_speedup_std, label='Weak efficiency')
+    ax2.plot(workers, strong_efficiency, '-.', label='Strong efficiency')
+    ax2.plot(workers, weak_efficiency, ':', label='Weak efficiency')
 
     ax1.legend(loc=2)
     ax2.legend(loc=1)
-    fig.savefig(fig_name + '.png')
+    fig.savefig(executable.name.replace(' ', '_') + '_full_scaling.png')
     
-def scalability_main():
+def scalability_main(test_funcs):
     if len(sys.argv[1:]) == 0:
         raise SystemExit('No test name given\nUsage:qsub %s -F test_name' % sys.argv[0])
 
     for test in sys.argv[1:]:
         try:
-            exec('__main__.' + test.strip() + '()')
-        except NameError:
-            raise SystemExit('Wrong test name: %s' % test)
+            test_funcs[test.strip()]()
+        except KeyError:
+            raise SystemExit('Wrong test name: ' + str(test))
 
